c762432a30742016979f3e3a92fa193945e8c18a
whwoohw <woojoo1114@naver.com>
2022-10-22 19:45:54 +0900

    update crud, add place model

--- reservation/views.py
+++ reservation/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from account.models import User
 from reservation.serializers import ReservationSerializer
-from studio.models import AssignedTime
+from studio.models import AssignedTime, Place, Studio
 from reservation.models import Reservation
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -9,110 +9,71 @@ from rest_framework import status
 
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
-import json
 
-
-# class ViewWithoutCSFRAuthentication(View):
-#     @method_decorator(csrf_exempt)
-#     def dispatch(self, request, *args, **kwargs):
-#         return super(ViewWithoutCSFRAuthentication, self).dispatch(request, *args, **kwargs)
-
-
-# status code 랑 JsonResponse 부분 다시 점검
-# Create your views here.
-
-# class ReservationCreateView(ViewWithoutCSFRAuthentication):
-#     def post(self, request):
-#         try:
-#             body = json.loads(request.body)
-#         except:
-#             return JsonResponse({"error": "Invalid parameters"}, status = 400)
-        
-#         try:
-#             user = User.objects.get(id=body['user_id'])
-#             assigned_time = AssignedTime.objects.get(id = body['time_id'])
-#             Reservation.objects.create(user=user, assigned_time=assigned_time, description=body['description'] )
-#         except:
-#             return JsonResponse({"error": "Failed to create Reservation"}, status = 400)
-        
-#         return JsonResponse({"success":"Reservation created successfully!"}, status = 201)
-
-# class ReservationStateView(ViewWithoutCSFRAuthentication):
-#     def post(self, request):
-#         try:
-#             body = json.loads(request.body)
-#         except:
-#             return JsonResponse({"error": "Invalid parameters"}, status = 400)
-
-#         try:
-#             user = User.objects.get(id=body['user_id'])
-#             reservation = Reservation.objects.get(id=body['reservation_id'])
-#             # 유저 그룹이 Normal group 인 유저는 액세스 불가 처리 .
-#             if user.groups.filter(name='NormalUser'):
-#                 if body['state'] == 'confirm':
-#                     return JsonResponse({"error": "Unallowed Access for NormalUser"}, status = 403)
-#                 else:
-#                     reservation.state_change(body['state'])
-#                 return JsonResponse({"success": f"reservation_{reservation.id}'s state is changed successfully."}, status =200)
-#             else:
-#                 # body['state'] 에는 confirmed, unconfirmed, canceled 정보가 필요함.
-#                 reservation.state_change(body['state'])
-#                 return JsonResponse({"success": f"reservation_{reservation.id}'s state is changed successfully."}, status =200)
-#         except:
-#             return JsonResponse({"error": "Failed to change reservation state"}, status=400)
 
 class AllReservationView(APIView):
-    def get(self, request): 
+    def get(self, request, studio_id): 
         try:
-            user = User.objects.get(id=request.GET.get('user_id'))
-            assigned_time = AssignedTime.objects.get(id=request.GET.get('assigned_time_id'))
-            reservation = Reservation.objects.get(user=user, assigned_time=assigned_time)
+            studio = Studio.objects.get(id=studio_id)
+            reservation = Reservation.objects.get(studio=studio)
+            if(request.data.get('state')):
+                try:
+                    state = request.data.get('state')
+                    reservation = reservation.objects.filter(state=state)
+                except:
+                    return Response({'error' : "fail to get reservation by state"}, status=status.HTTP_400_BAD_REQUEST)
+            if(request.data.get('place_id')):
+                try:
+                    place = Place.objects.get(id=request.data.get('place_id'))
+                    reservation = reservation.objects.filter(place=place)
+                except:
+                    return Response({'error' : "fail to get reservation by place"}, status=status.HTTP_400_BAD_REQUEST)
             serializer = ReservationSerializer(reservation, many=True)
-            return Response(serializer.data)
+            return Response({'data' : serializer.data, 'success' : 'get all reservation'})
         
         except:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return Response({'error' : 'get all reservation'}, status=status.HTTP_404_NOT_FOUND)
     
-    def post(self, request):
+    def post(self, request, studio_id):
         try:
             user = User.objects.get(id=request.data.get('photographer_id'))
             assigned_time = AssignedTime.objects.get(id=request.data.get('assignedtime_id'))
             description = request.data.get('description')
             reservation = Reservation.objects.create(user=user, assigned_time=assigned_time, description=description)
             serializer = ReservationSerializer(reservation)
-            return Response(serializer.data)
+            return Response({'data' : serializer.data, 'success' : 'post reservation'})
         
         except:
-            return Response(status=status.HTTP_400_BAD_REQUEST)      
+            return Response({'error' : 'post reservation'}, status=status.HTTP_400_BAD_REQUEST)      
 
 class ReservationView(APIView):
-    def get(self, request, id):
+    def get(self, request, id, studio_id):
         try:
             reservation = Reservation.objects.get(id=id)
             serializer = ReservationSerializer(reservation)
-            return Response(serializer.data)
+            return Response({'data' : serializer.data, 'success' : 'get reservation'})
         
         except:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+            return Response({'error' : 'get reservation'}, status=status.HTTP_400_BAD_REQUEST)
     
-    def patch(self, request, id):
+    def patch(self, request, id, studio_id):
         try:
             reservation = Reservation.objects.get(id=id)
             serializer = ReservationSerializer(reservation, data=request.data, partial=True)
             if serializer.is_valid():
                 serializer.save()
-                return Response(serializer.data)
+                return Response({'data' : serializer.data, 'success' : 'patch reservation'})
             
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+            return Response({'error' : 'patch reservation invalid form'}, status=status.HTTP_400_BAD_REQUEST)
         
         except:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return Response({'error' : 'patch reservation'}, status=status.HTTP_404_NOT_FOUND)
     
-    def delete(self, request, id):
+    def delete(self, request, id, studio_id):
         try:
             reservation = Reservation.objects.get(id=id)
             reservation.delete()
-            return Response(status=status.HTTP_200_OK)
+            return Response({'success' : 'delete reservation'}, status=status.HTTP_200_OK)
         
         except:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+            return Response({'error' : 'delete reservation'}, status=status.HTTP_400_BAD_REQUEST)
