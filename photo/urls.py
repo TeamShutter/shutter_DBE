d82f342fe0b3c7a44434f03226107d656337750b
노현호 <nohhyunho@nohyeonhoui-MacBookPro.local>
2022-10-25 22:29:33 +0900

    사진 한장씩 가는 photoview

--- photo/urls.py
+++ photo/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import AllPhotoView
+from .views import AllPhotoView, PhotoView
 
 urlpatterns = [
     path("", AllPhotoView.as_view(), name="all_photos"),
-    # path("<int:pid>/", GetPhoto),
+    path("<int:photo_id>/", PhotoView.as_view(), name='single_photo'),
     # path("<int:pid>/like/", LikePhoto)
 ]
 
