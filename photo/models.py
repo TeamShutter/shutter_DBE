6672859f00b1d33487b15e683e50998babea97bb
whwoohw <woojoo1114@naver.com>
2022-11-08 19:34:50 +0900

    add price

--- photo/models.py
+++ photo/models.py
@@ -14,6 +14,7 @@ class Photo(models.Model):
     studio = models.ForeignKey(Studio, null = True, on_delete=models.CASCADE)
     #address 는 외래키, 태그는 다대다(하나의 사진에서 여러개의 태그를 등록할 수 있도록)
     like_users = models.ManyToManyField(User, blank=True, related_name='like_photos', through='Like')
+    price = models.IntegerField(blank=True, null=True)
 
     class Meta:
         db_table = "photo"
