536297c7ca27827f74e1067698ff8f303087d850
whwoohw <woojoo1114@naver.com>
2022-11-12 00:15:04 +0900

    색감 추가 및 연관 사진 출력 작업중

--- photo/urls.py
+++ photo/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import AllPhotoView, LikePhotoView, PhotoView
+from .views import AllPhotoView, LikePhotoView, PhotoView, RelatedPhotoView
 
 urlpatterns = [
     path("", AllPhotoView.as_view(), name="all_photos"),
     path("<int:photo_id>/", PhotoView.as_view(), name='single_photo'),
     path("<int:photo_id>/like/", LikePhotoView.as_view(), name='like_photo'),
+    # path("color/", PhotoColorView.as_view(), name="all_colors"),
+    path("<int:photo_id>/related_photos/", RelatedPhotoView.as_view(), name='related_photos'),
 ]
 
