e2ff2d5af56733577cf24aaa7448c43255291136
hyunho <hhnoh98@naver.com>
2022-11-26 21:10:09 +0900

    studio order edited

--- studio/admin.py
+++ studio/admin.py
@@ -3,7 +3,7 @@
 from django.contrib import admin
 
 from studio.models import Place, Studio, Product, OpenedTime, Photographer, AssignedTime, StudioImage
-
+# from studio.models import StudioTime, Time, StudioMerchandise, Merchandise
 # Register your models here.
 admin.site.register(Studio)
 admin.site.register(Product)
@@ -12,4 +12,9 @@ admin.site.register(Photographer)
 admin.site.register(AssignedTime)
 admin.site.register(Place)
 admin.site.register(StudioImage)
+
+# admin.site.register(StudioTime)
+# admin.site.register(Time)
+# admin.site.register(StudioMerchandise)
+# admin.site.register(Merchandise)
 
